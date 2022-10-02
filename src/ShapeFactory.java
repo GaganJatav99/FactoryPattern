c71bd65af1d0d3128926650637fb6ee879b7ec7c
charizard99 <gagan280799@gmail.com>
2022-10-02 15:33:53 +0530

    Factory Pattern

---
+++ src/ShapeFactory.java
@@ -0,0 +1,22 @@
+public class ShapeFactory {
+
+
+    Shape getShape(String input){
+
+        switch (input){
+
+            case "CIRCLE":
+                return new Circle();
+            case "RECTANGLE":
+                return new Rectangle();
+            case "SQUARE":
+                return new Square();
+            default:
+                return null;
+
+        }
+
+    }
+
+
+}
